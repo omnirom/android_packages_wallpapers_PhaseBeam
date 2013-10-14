144a0be655b4894dab4571dbf71b2a92edfedc25
xplodwild <xplod@ouverta.fr>
2013-10-14 22:53:42 +0100

    PhaseBeamSelector: Make license of that file clearer
    - Brings back license of the original unmodified file
    - GPLv3 applies only to the changes made by myself in
      the previous commits to this file
    
    Change-Id: Iafe166c8836da5f86b7c166518f5e6c84fc8dbac

--- src/com/android/phasebeam/PhaseBeamSelector.java
+++ src/com/android/phasebeam/PhaseBeamSelector.java
@@ -1,5 +1,22 @@
 /*
- * Copyright (C) 2013 The OmniROM Project
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Per article 5 of the Apache 2.0 License, some modifications to this code
+ * were made by the OmniROM Project.
+ *
+ * Modifications Copyright (C) 2013 The OmniROM Project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
